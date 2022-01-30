05875b352a7af134f10b03b77149403243afccf3
Igor Mont <igornathandev@gmail.com>
2022-01-30 15:32:07 -0300

    Resolving promises with 'await' and fix Query user

--- src/graphql/modules/tasks/resolvers.js
+++ src/graphql/modules/tasks/resolvers.js
@@ -2,11 +2,11 @@ import { Task } from "../../../database/models/Task";
 
 export default {
   Query: {
-    tasks: async () => Task.find()
+    tasks: async () => await Task.find()
   },
   Mutation: {
-    createTask: async (_, { data }) => Task.create(data),
-    updateTask: async (_, { id, data }) => Task.findOneAndUpdate(id, data, { new: true }),
-    deleteTask: async (_, { id }) => Task.findOneAndDelete(id),
+    createTask: async (_, { data }) => await Task.create(data),
+    updateTask: async (_, { id, data }) => await Task.findOneAndUpdate(id, data, { new: true }),
+    deleteTask: async (_, { id }) => await Task.findOneAndDelete(id),
   }
 }
--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -2,11 +2,11 @@ import { User } from "../../../database/models/User";
 
 export default {
   Query: {
-    user: async () => User.find()
+    user: async (__, { id }) => await User.findById(id)
   },
   Mutation: {
-    createUser: async (_, { data }) => User.create(data),
-    updateUser: async (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
-    deleteUser: async (_, { id }) => User.findOneAndDelete(id),
+    createUser: async (_, { data }) => await User.create(data),
+    updateUser: async (_, { id, data }) => await User.findOneAndUpdate(id, data, { new: true }),
+    deleteUser: async (_, { id }) => await User.findOneAndDelete(id),
   }
 }
