192fa506fd981e788e2fe27217ef670d9f05ed9b
Igor Mont <igornathandev@gmail.com>
2022-01-31 10:37:15 -0300

    Ad query users and resolve return boolean of deleteUser

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -2,11 +2,12 @@ import { User } from "../../../database/models/User";
 
 export default {
   Query: {
-    user: async (__, { id }) => await User.findById(id)
+    users: async () => await User.find(),
+    user: async (_, { id }) => await User.findById(id)
   },
   Mutation: {
     createUser: async (_, { data }) => await User.create(data),
     updateUser: async (_, { id, data }) => await User.findOneAndUpdate(id, data, { new: true }),
-    deleteUser: async (_, { id }) => await User.findOneAndDelete(id),
+    deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
   }
 }
